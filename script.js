b59fcd6b3fa60d065374da373ad6b9e2d1ef97a8
yuyudatai667 <167619477+yuyudatai667@users.noreply.github.com>
2024-05-16 00:27:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-// JavaScript:
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("风自东来的博客已加载完成");
-    // 在这里添加更多 JS 代码进行操作
-});
+// Your scripts here. For the static page, scripts might not be necessary.
